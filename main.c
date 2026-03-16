640815ccb3bc77f5f5b1fd2090cce22f1f24d4fe
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 11:25:33 -0300

    terceiroo

--- main.c
+++ main.c
@@ -6,5 +6,6 @@ int main() {
     printf("Vitor aqui! Terceiro commit\n");
     printf("Pierre aqui! Primeiro commit\n");
     printf("Pierre aqui! Segundo commit\n");
+    printf("Pierre aqui! Terceiro commit\n");
     return 0;
 }
