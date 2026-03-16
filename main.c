8f7e2b286d593acbe0b95a2333f4c584aa62c5ca
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 11:29:58 -0300

    fiz merda, apaguei o printf do pierre

--- main.c
+++ main.c
@@ -5,7 +5,5 @@ int main() {
     printf("Vitor aqui! Segundo commit\n");
     printf("Vitor aqui! Terceiro commit\n");
     printf("Pierre aqui! Primeiro commit\n");
-    printf("Pierre aqui! Segundo commit\n");
-    printf("Pierre aqui! Terceiro commit\n");
     return 0;
 }
