851de898695bae5f7472812c2a80f7422c0704b5
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 11:32:21 -0300

    meu segundo commit

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main() {
     printf("Vitor aqui! Segundo commit\n");
     printf("Vitor aqui! Terceiro commit\n");
     printf("Pierre aqui! Primeiro commit\n");
+    printf("Matheus aqui! Segundo commit\n");
     return 0;
 }
